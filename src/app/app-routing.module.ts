ce4b475d8fb09a958480282e57109fe3f3c78f4a
Dillon Stock <stockdillon@yahoo.com>
2020-03-11 20:40:02 -0400

    build successful; bootstrap styles missing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { RocketPageComponent } from './pages/rocket-page/rocket-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rocket', component: RocketPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    PagesModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
